ace3490bc5f57e86791f8fe669f8b8a31b04b36a
surodeep26 <108617248+surodeep26@users.noreply.github.com>
2023-10-05 19:35:36 +0530

    Add files via upload

---
+++ fundamentals/script.js
@@ -0,0 +1,23 @@
+function showDefaultTopic() {
+    var defaultTopic = document.getElementById('topic3');
+    if (defaultTopic) {
+        defaultTopic.style.display = 'block';
+    }
+}
+
+
+function showContent(topic, event) {
+    event.preventDefault(); // Prevent the default behavior (scrolling)
+
+    var allContent = document.querySelectorAll('.topic-content');
+    allContent.forEach(function (element) {
+        element.style.display = 'none';
+    });
+
+    var selectedContent = document.getElementById(topic);
+    if (selectedContent) {
+        selectedContent.style.display = 'block';
+    }
+}
+
+showDefaultTopic();
